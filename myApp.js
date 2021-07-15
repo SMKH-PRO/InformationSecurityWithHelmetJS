33f1813e4e4e11ff0723ba30a22245407915a989
KASHAN HAIDER <smkh.kashan@gmAIL.COM>
2021-07-15 18:36:58 +0500

    dnsPrefetchControl

--- myApp.js
+++ myApp.js
@@ -54,6 +54,16 @@ app.use(
   })
 );
 
+
+/**
+ To improve performance, most browsers prefetch DNS records for the links in a page. In that way the destination ip is already known when the user clicks on a link. This may lead to over-use of the DNS service (if you own a big website, visited by millions people…), privacy issues (one eavesdropper could infer that you are on a certain page), or page statistics alteration (some links may appear visited even if they are not). If you have high security needs you can disable DNS prefetching, at the cost of a performance penalty.
+
+ Use the helmet.dnsPrefetchControl() method on your server.
+ **/
+
+ app.use(
+  helmet.dnsPrefetchControl()
+);
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
