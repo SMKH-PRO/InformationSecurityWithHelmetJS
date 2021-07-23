1b1f6ee2e89c93a7399124b69b7e399836396580
KASHAN HAIDER <smkh.kashan@gmAIL.COM>
2021-07-23 18:06:18 +0500

    w

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const helmet = require("helmet");
+
+helmet.hidePoweredBy()
+
 app.use(helmet());
 
+module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
@@ -14,3 +18,4 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+
