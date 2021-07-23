bbadf09c73efb3adc530e09e87793298ac525f1f
KASHAN HAIDER <smkh.kashan@gmAIL.COM>
2021-07-23 18:29:46 +0500

    ieNoOpen

--- myApp.js
+++ myApp.js
@@ -5,16 +5,41 @@ const helmet = require("helmet");
 
 
 app.use(helmet());
+
+/**
+Hackers can exploit known vulnerabilities in Express/Node if they see that your site is powered by Express. X-Powered-By: Express is sent in every request coming from Express by default. Use the helmet.hidePoweredBy() middleware to remove the X-Powered-By header.
+ **/
 app.use(helmet.hidePoweredBy())
+
+/**
+Your page could be put in a <frame> or <iframe> without your consent. This can result in clickjacking attacks, among other things. Clickjacking is a technique of tricking a user into interacting with a page different from what the user thinks it is. This can be obtained executing your page in a malicious context, by mean of iframing. In that context a hacker can put a hidden layer over your page. Hidden buttons can be used to run bad scripts. This middleware sets the X-Frame-Options header. It restricts who can put your site in a frame. It has three modes: DENY, SAMEORIGIN, and ALLOW-FROM.
+We don’t need our app to be framed.
+ **/
 app.use(
   helmet.frameguard({
     action: "deny",
   })
 );
 
+
+/**
+Cross-site scripting (XSS) is a frequent type of attack where malicious scripts are injected into vulnerable pages, with the purpose of stealing sensitive data like session cookies, or passwords.
+The basic rule to lower the risk of an XSS attack is simple: “Never trust user’s input”. As a developer you should always sanitize all the input coming from the outside. This includes data coming from forms, GET query urls, and even from POST bodies. Sanitizing means that you should find and encode the characters that may be dangerous e.g. <, >.
+Modern browsers can help mitigating the risk by adopting better software strategies. Often these are configurable via http headers.
+The X-XSS-Protection HTTP header is a basic protection. The browser detects a potential injected script using a heuristic filter. If the header is enabled, the browser changes the script code, neutralizing it. It still has limited support.
+ **/
 app.use(helmet.xssFilter());
+
+
+/**
+ Browsers can use content or MIME sniffing to override response Content-Type headers to guess and process the data using an implicit content type. While this can be convenient in some scenarios, it can also lead to some dangerous attacks. This middleware sets the X-Content-Type-Options header to nosniff, instructing the browser to not bypass the provided Content-Type.
+ **/
 app.use(helmet.noSniff());
 
+/**
+  Some web applications will serve untrusted HTML for download. Some versions of Internet Explorer by default open those HTML files in the context of your site. This means that an untrusted HTML page could start doing bad things in the context of your pages. This middleware sets the X-Download-Options header to noopen. This will prevent IE users from executing downloads in the trusted site’s context.
+**/
+app.use(helmet.ieNoOpen());
 
 module.exports = app;
 const api = require('./server.js');
