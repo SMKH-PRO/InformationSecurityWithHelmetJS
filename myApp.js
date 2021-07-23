205b0ab072b14f2746e47585d2e0a0a0b88d1902
KASHAN HAIDER <smkh.kashan@gmAIL.COM>
2021-07-23 18:19:04 +0500

    helmet.frameguard()

--- myApp.js
+++ myApp.js
@@ -6,6 +6,13 @@ const helmet = require("helmet");
 
 app.use(helmet());
 app.use(helmet.hidePoweredBy())
+app.use(
+  helmet.frameguard({
+    action: "deny",
+  })
+);
+
+
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
